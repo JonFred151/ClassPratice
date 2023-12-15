01c177b3607f5f38510b7a6b732599836b4911f9
Jon Frederick <JonFred151@gmail.com>
2023-12-15 10:00:21 -0600

    Pratice of Class

---
+++ ClassPratice/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ClassPratice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Worlds = new Team();
+
+            Worlds.TrainerName = "Wolfe";
+            Worlds.PokeName = new string[]  { "Pikachu" };
+            Worlds.stadium = "Orlando";
+            Worlds.round = 2;
+            Worlds.Draft("Pikachu", "Wolfe");
+
+            Worlds.match();
+
+
+        }
+    }
+
+}
---
+++ ClassPratice/Team.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassPratice
+{
+    internal class Team
+    {
+        public string TrainerName { get; set; }
+        public string[] PokeName {  get; set; }
+        public string stadium {  get; set; } 
+        public double round {  get; set; }  
+
+        public void Draft(string PokemonName, string PokeTrainer)
+        {
+            Console.WriteLine($"{PokemonName} has been chosen by {PokeTrainer}");
+        }
+
+        public void match()
+        {
+            Console.WriteLine($" Trainer {TrainerName} is playing at {stadium} and the is round {round} using {PokeName}");
+        }
+    }
+}
